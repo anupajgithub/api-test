bd266c69c8ee58bfe1665eae50e07d9783b8ebb6
aj <anupjena529@gmail.com>
2022-12-30 16:34:21 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./style.css";
+import data from "./TemplateData.json";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="templateContainer">
+        <div className="searchInput_Container">
+          <input
+            id="searchInput"
+            type="text"
+            placeholder="Search here..."
+            onChange={(event) => {
+              setSearchTerm(event.target.value);
+            }}
+          />
+        </div>
+        <div className="template_Container">
+          {data
+            .filter((val) => {
+              if (searchTerm == "") {
+                return val;
+              } else if (
+                val.title.toLowerCase().includes(searchTerm.toLowerCase())
+              ) {
+                return val;
+              }
+            })
+            .map((val) => {
+              return (
+                <div className="card" key={val.id}>
+                  <div className="card_img">
+                    <img src={val.image} />
+                  </div>
+                  <div className="card_header">
+                    <h2>{val.title}</h2>
+
+                    <p>{val.description}</p>
+                    <p>{val.category}</p>
+
+                    <p>{val.price}</p>
+                    <div className="btn">Add to cart</div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </>
   );
 }
 
